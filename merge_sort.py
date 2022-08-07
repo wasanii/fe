c9777f08fa5d790b29d7a57322cc459926a33da6
wasanii <wasanii@ymail.com>
2022-08-07 17:36:51 +0900

    マージソートをリファクタリング

--- merge_sort.py
+++ merge_sort.py
@@ -4,61 +4,45 @@ import math
 def sort():
     nums = [6, 4, 3, 7, 5, 1, 2]
 
-    print(f'nums: {nums}')
+    print(f'nums: {nums}\n')
     result = merge_sort(nums)
     print(f'\nsorted: {result}')
 
 
-def merge_sort(nums):
+def merge_sort(nums):  # 併合フェーズ
+    print(f'nums: {nums}')
+
     length = len(nums)
-    result = nums
 
-    for level in range(math.ceil(math.log2(length))):   # O(n*log2(n))とよくわかる
-        size = 2 ** level  # その段で比較するleft, rightの要素数  ※ ^ 記号はXORなので、2 ^ levelだと2 XOR 0で2になる！
-        print(f'\nlevel: {level}')
-        print(f'size: {size}')
-        merged = []
-        for i in range(0, length, size * 2):    # 下から1段ずつ処理する
-            left_s = i          # 左右の組を作ってmergeする
-            left_e = i + size
-            right_e = min(left_e + size, length)
-            merged = merged + merge(result[left_s: left_e], result[left_e: right_e])
+    if length == 1:  # 1コだけなら終了
+        return nums
 
-        result = merged
-        print(f'result: {result}')
+    mid = length // 2  # 半分に分ける
+    left = merge_sort(nums[:mid])  # 左半分をマージソート
+    right = merge_sort(nums[mid:])  # 右半分をマージソート
+
+    result = merge(left, right)  # マージソートしたものを併合する
+
+    # print(f'result: {result}')
+
     return result
 
 
-def merge(left, right):
+def merge(left, right):  # 分割統治フェーズ
     print(f'left: {left}, right: {right}')
-    if len(right) == 0:  # right == [] とはできなかったと思う
-        print(f'merged: {left}')
-        return left
 
-    merged = []
+    right.reverse()  # [5, 6], [2, 7] → [5, 6, 7, 2] にすると
+    work = left + right  # 比較対象が先頭と末尾になるので比較しやすい
+    result = []
 
-    # left_i = 0
-    # right_i = 0
-    # for i in range(len(left) + len(right)):
-    #     if left[left_i] < right[right_i]:
-    #         merged.append(left[left_i])
-    #         left_i += 1
-    #     else:
-    #         merged.append(right[right_i])
-    #         right_i += 1
-    #     if (left_i == len(left)) or (right_i == len(right)):
-    #         break
-
-    while (len(left) > 0) and (len(right) > 0):
-        if left[0] < right[0]:
-            merged.append(left.pop(0))
+    while len(work) > 0:
+        if work[0] < work[-1]:  # 左端 < 右端 なら 左端を結果の配列に詰めていく
+            result.append(work.pop(0))
         else:
-            merged.append(right.pop(0))
+            result.append(work.pop())
 
-    merged = merged + left + right
-
-    print(f'merged: {merged}')
-    return merged
+    print(f'merged: {result}')
+    return result
 
 
 if __name__ == '__main__':
